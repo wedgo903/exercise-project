3104b8a8211684df1134aeda404ccb3b0ee363a2
wedgo903 <awolanin160359@gmail.com>
2024-07-06 13:31:55 +0200

    Dodaj flagę --help do wyświetlania dostępnych opcji

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,4 @@ case $1 in
   *) echo "Invalid option" ;;
 esac
   --logs) count=${2:-100}; for i in $(seq 1 $count); do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
+  --help) echo "Dostępne opcje: --date, --logs [N], --help" ;;
