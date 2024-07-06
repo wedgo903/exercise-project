b966c213a9d3f654c3921c73e5454db216fa3a87
wedgo903 <awolanin160359@gmail.com>
2024-07-06 13:10:13 +0200

    Dodaj flagę --logs N do tworzenia określonej liczby plików log

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,4 @@ case $1 in
   *) echo "Invalid option" ;;
 esac
   --logs) for i in {1..100}; do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
+  --logs) count=${2:-100}; for i in $(seq 1 $count); do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
