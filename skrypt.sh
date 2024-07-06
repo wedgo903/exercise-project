84703fadad163ed6ddea22214e7cec0c87abe702
wedgo903 <awolanin160359@gmail.com>
2024-07-06 13:15:04 +0200

    Dodaj flagi z pojedynczym myślnikiem -d, -l, -h

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 case $1 in
-  --date) date ;;
+  --date|-d) date ;;
   *) echo "Invalid option" ;;
 esac
-  --logs) for i in {1..100}; do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
-  --logs) count=${2:-100}; for i in $(seq 1 $count); do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
-  --help) echo "Dostępne opcje: --date, --logs [N], --help" ;;
+  --logs|-l) for i in {1..100}; do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
+  --logs|-l) count=${2:-100}; for i in $(seq 1 $count); do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
+  --help|-h) echo "Dostępne opcje: --date, --logs [N], --help" ;;
