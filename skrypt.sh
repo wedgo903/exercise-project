d5f2ad7042b7275028505fd91546b31e88318ac6
wedgo903 <awolanin160359@gmail.com>
2024-07-06 13:35:44 +0200

    Dodaj flagę --error do tworzenia plików error oraz flagę -e

--- skrypt.sh
+++ skrypt.sh
@@ -6,3 +6,5 @@ esac
   --logs|-l) count=${2:-100}; for i in $(seq 1 $count); do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
   --help|-h) echo "Dostępne opcje: --date, --logs [N], --help" ;;
   --init) git clone https://github.com/wedgo903/exercise-project.git; export PATH=$PATH:$(pwd)/exercise-project ;;
+  --error) count=${2:-100}; mkdir -p error; for i in $(seq 1 $count); do echo -e "error$i/error$i.txt\nskrypt.sh\n$(date)" > error/error$i.txt; done ;;
+  -e) count=${2:-100}; mkdir -p error; for i in $(seq 1 $count); do echo -e "error$i/error$i.txt\nskrypt.sh\n$(date)" > error/error$i.txt; done ;;
