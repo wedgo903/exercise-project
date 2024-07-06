4241fdb9ec90b417807325334fd4315eb3ed3db2
wedgo903 <awolanin160359@gmail.com>
2024-07-06 13:35:35 +0200

    Dodaj flagę --init do klonowania repozytorium i ustawiania PATH

--- skrypt.sh
+++ skrypt.sh
@@ -5,3 +5,4 @@ case $1 in
 esac
   --logs|-l) count=${2:-100}; for i in $(seq 1 $count); do echo -e "log$i.txt\nskrypt.sh\n$(date)" > log$i.txt; done ;;
   --help|-h) echo "Dostępne opcje: --date, --logs [N], --help" ;;
+  --init) git clone https://github.com/wedgo903/exercise-project.git; export PATH=$PATH:$(pwd)/exercise-project ;;
